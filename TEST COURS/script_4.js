650e66ad571abdb71f356d0e65d345096e05066d
laurent784 <pruvot.laurent80@orange.fr>
2023-05-09 17:50:48 +0200

    EXO-script Opérateur

---
+++ TEST COURS/script_4.js
@@ -0,0 +1,10 @@
+var a = "100"; // Ceci est une chaîne de Caractère avec les quotes
+var b = 100; // Ceci est un nombre sans les quotes
+var c = 1.00;
+var d = true; // Ceci est une valeur Booléene Vrai
+console.log("Ceci est une chaine de caractères:" + a);
+var dec = b - 1; // On applique la valeur de b à la décrémentation dans la console
+console.log(dec);
+var ajout = c + a;  // On ajoute en concaténant c à la valeur de a
+console.log(ajout); // On affiche c à la valeur de a dans la console
+console.log(!d); // On affiche l'inverse de la valeur de Vrai pour avoir la valeur Faux à l'aide du signe (!) dans la console
