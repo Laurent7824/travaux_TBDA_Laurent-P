0050fab8bacf03ae23506c13e213ad85e777379d
laurent784 <pruvot.laurent80@orange.fr>
2023-05-09 12:13:30 +0200

    EXO 1-script Afficher du texte

---
+++ TEST COURS/script.js
@@ -0,0 +1,8 @@
+// Pour savoir si la console est connecté au script
+console.log("connecté !!!");  
+// Déclare la variable nom et affiche le "message" dans une boîte de dialogue
+var nom = prompt("Saisissez votre nom");
+// Déclare la variable prénom et affiche le "message" dans une boîte de dialogue
+var prenom = prompt("Saisissez votre prénom");
+// Affiche les informations optionnelle dans une boîte de dialogue
+alert(nom+"_"+prenom);
