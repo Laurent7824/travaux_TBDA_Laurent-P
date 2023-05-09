510de1b95a647879bec67b6d22b1be465e78ab24
laurent784 <pruvot.laurent80@orange.fr>
2023-05-09 15:54:39 +0200

    EXO 2-script Afficher du texte

---
+++ TEST COURS/script_2.js
@@ -0,0 +1,5 @@
+console.log("connecté !!!");
+var a = prompt("Entrez le premier nombre");
+var b = prompt("Entrer le deuxième nombre");
+var produit = parseInt(a) * parseInt(b);
+document.write("Le produit de "+a+" et "+b+" égal à "+ produit);
