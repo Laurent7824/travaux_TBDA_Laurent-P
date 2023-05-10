77531589b0a6b597ae1fe4bdabf307f6b769e527
laurent784 <pruvot.laurent80@orange.fr>
2023-05-10 12:54:28 +0200

    La Parité

---
+++ TEST COURS/script_5.js
@@ -0,0 +1,12 @@
+console.log('connecté !')
+function check() {
+    var nbr;
+    nbr = Number(document.getElementById("myInput").value);
+    if (nbr % 2 == 0) {
+        alert("Nombre pair");
+    }
+    else {
+        alert("Nombre impair");
+    }
+}
+
