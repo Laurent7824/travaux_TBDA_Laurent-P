771f58c3c3badf93e416db99c993aae2877b578f
laurent784 <pruvot.laurent80@orange.fr>
2023-05-16 15:41:23 +0200

    Ajout de commentaire

--- Evaluation/exo1.js
+++ Evaluation/exo1.js
@@ -1,23 +1,25 @@
+// Déclarations des variables
 var age = 0;
 var nbMoins20 = 0;
 var nbEntre20et40 = 0;
 var nbPlus40 = 0;
 
+// J'utilise une boucle
 while (age >= 0) {
 
-    age = parseInt(prompt("Entrez un âge :"));
-    if (age < 20) {
-        nbMoins20++;
+    age = parseInt(prompt("Entrez un âge :")); // Je convertie un string (Chaîne de caractère) en un nombre entier
+    if (age < 20) { // Si l'âge est inférieur à 20
+        nbMoins20++; // On incrémente de 1 la valeur de la variable nbMoins20
     }
-    else if (age >= 20 && age <= 40) {
-        nbEntre20et40++;
+    else if (age >= 20 && age <= 40) { // Sinon si l'âge est compris entre 20 et 40 ans
+        nbEntre20et40++; // On incrémente de 1 la valeur de la variable nbEntre20et40
     }
-    else if (age > 40 && age < 100) {
-        nbPlus40++;
+    else if (age > 40 && age < 100) { // Sinon si l'âge est compris entre 40 et 100 ans
+        nbPlus40++; // On incrémente de 1 la valeur de la variable nbPlus40
     }
     else {
         nbPlus40++;
-        break;
+        break; // Fin de la boucle
     }
 
 }
--- Evaluation/exo2.js
+++ Evaluation/exo2.js
@@ -1,8 +1,13 @@
-console.log("connecté");
+// Déclaration de la variable (nombre)
+// Je convertie le string en nombre entier
 var nombre = parseInt(prompt("Saisir un nombre ?"));
+
+// Déclaration de la fonction avec le paramètre (nombre)
+/*J'utilise une boucle pour que la variable i vaut 1, sachant que i soit inférieureou égale à 10
+on incrémentera i de 1*/
 function TableMultiplication(nombre) {
     for (var i = 1; i <= 10; i++) {
-        console.log(nombre + "X" + i + "=" + (nombre * i));
+        console.log(nombre + "X" + i + "=" + (nombre * i)); // On affiche dans la console le calcul du produit
     }
 }
-TableMultiplication(nombre);
+TableMultiplication(nombre); // On appel la fonction (nombre)
